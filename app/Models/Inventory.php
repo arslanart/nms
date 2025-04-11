303bb951f529322d378ad18e6f47df628284d649
arslanart <arslanart913@gmail.com>
2025-04-11 16:56:25 +0700

    📝 เพิ่มการใช้งาน SoftDeletes ใน Model 🗑️

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Inventory extends Model
 {
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'id',
+        'group_id',
+        'name',
+        'description',
+        'serial_number',
+        'model_number',
+        'mac_address',
+        'ip_address',
+        'port',
+        'multicast_address',
+    ];
     //
 }
