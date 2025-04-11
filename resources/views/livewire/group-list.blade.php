2e1a56959b8d9ffff3c988ce152639492ba1066e
arslanart <arslanart913@gmail.com>
2025-04-11 15:20:17 +0700

    👷ปรับปรุงโครงสร้างและรูปแบบหน้า Group List โดยรวมส่วนหัวและปรับปรุงการแสดงข้อมูลอุปกรณ์🔧

--- resources/views/livewire/group-list.blade.php
+++ resources/views/livewire/group-list.blade.php
@@ -1,4 +1,3 @@
-
 <div>
     <div class="content-wrapper">
         <section class="content-header">
@@ -12,72 +11,61 @@
                             <i class="fas fa-plus"></i> Add Group
                         </a>
                     </div>
-<div class="content-wrapper">
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>Group</h1>
+                    <div class="content-wrapper">
+                        <!-- Main content -->
+                        <section class="content">
+                            <div class="card">
+                                <div class="card-body p-0">
+                                    <table class="table table-striped table-bordered">
+                                        <thead class="bg-dark-secondary text-dark">
+                                            <tr>
+                                                <th style="width: 10%"scope="col" class="text-center">No.</th>
+                                                <th scope="col" class="text-center">Group</th>
+                                                <th scope="col" class="text-center">Multicast address</th>
+                                                <th scope="col" class="text-center">Port</th>
+                                                <th scope="col" class="text-center">Select</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @if (!empty($data) && $data->isNotEmpty())
+                                                @foreach ($data as $item)
+                                                    <tr>
+                                                        <td class="text-center">{{ $loop->iteration }}</td>
+                                                        <td class="text-center">{{ $item->group_name }}</td>
+                                                        <td class="text-center">{{ $item->multicast_address }}</td>
+                                                        <td class="text-center">{{ $item->port }}</td>
+                                                        <td class="text-center">
+                                                            <a href="{{ route('profile-view', ['id' => $item->id]) }}"
+                                                                class="btn btn-primary btn-sm">
+                                                                <i class="fas fa-eye"></i>
+                                                            </a>
+                                                            <a href="{{ route('profile-edit', ['id' => $item->id]) }}"
+                                                                class="btn btn-warning btn-sm">
+                                                                <i class="fas fa-edit"></i>
+                                                            </a>
+                                                            <button class="btn btn-danger btn-sm"
+                                                                wire:click.prevent="delete({{ $item->id }})">
+                                                                <i class="fas fa-trash"></i>
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            @else
+                                                <tr>
+                                                    <td colspan="5" class="text-center">No data available</td>
+                                                </tr>
+                                            @endif
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="card-footer">
+                                    {{ $data->links() }}
+                                </div>
+                            </div>
+                        </section>
+                    </div>
                 </div>
-                <div class="col-sm-6 text-right">
-                    <button class="btn btn-success" wire:click="createGroup">
-                        <i class="fas fa-plus"></i> Add Group
-                    </button>
-
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-        <div class="card">
-            <div class="card-body p-0">
-                <table class="table table-striped table-bordered">
-                    <thead class="bg-dark-secondary text-dark">
-                        <tr>
-                            <th style="width: 10%"scope="col" class="text-center">No.</th>
-                            <th scope="col" class="text-center">Group</th>
-                            <th scope="col" class="text-center">Multicast address</th>
-                            <th scope="col" class="text-center">Port</th>
-                            <th style="width: 12%" scope="col" class="text-center">Select</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @if (!empty($data) && $data->isNotEmpty())
-                            @foreach ($data as $item)
-                                <tr>
-                                    <td class="text-center">{{ $loop->iteration }}</td>
-                                    <td class="text-center">{{ $item->group_name }}</td>
-                                    <td class="text-center">{{$item->multicast_address }}</td>
-                                    <td class="text-center">{{ $item->port}}</td>
-                                    <td class="text-center">
-                                        <a href="{{ route('profile-view', ['id' => $item->id]) }}"
-                                            class="btn btn-primary btn-sm">
-                                            <i class="fas fa-eye"></i>
-                                        </a>
-                                        <a href="{{ route('profile-edit', ['id' => $item->id]) }}"
-                                            class="btn btn-warning btn-sm">
-                                            <i class="fas fa-edit"></i>
-                                        </a>
-                                        <button class="btn btn-danger btn-sm"
-                                            wire:click.prevent="delete({{ $item->id }})">
-                                            <i class="fas fa-trash"></i>
-                                        </button>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        @else
-                            <tr>
-                                <td colspan="5" class="text-center">No data available</td>
-                            </tr>
-                        @endif
-                    </tbody>
-                </table>
-            </div>
-            <div class="card-footer">
-                {{ $data->links('pagination::bootstrap-4') }}
-            </div>
-        </div>
-    </section>
+            </div><!-- /.container-fluid -->
+        </section>
+    </div>
 </div>
