df7e54c951c779e1df0c186bef2bf2ecc6ec6d0e
kongzang <gongpipop.2546@gmail.com>
2025-04-17 14:51:55 +0700

    📝 Update home-menu view structure for improved layout and content organization

--- resources/views/livewire/home-menu.blade.php
+++ resources/views/livewire/home-menu.blade.php
@@ -1,5 +1,18 @@
-<div>
-    <div class="content-wrapper">
-        <h1 style="text-align: center">In Progress...</h1>
-    </div>
+<!-- filepath: d:\laragon\www\NMS\resources\views\livewire\admin-menu.blade.php -->
+<div class="content-wrapper">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Home</h1>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+    </section>
 </div>
